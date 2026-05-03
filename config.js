a24092c797f96a4fdf8158768a573c56f1ed6b7d
IBRAhim-source-bliu <its3ibrahim3here@gmail.com>
2026-05-03 15:37:22 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+botName: "MODSXTOOLS MD",
+ownerName: "Ibrahim",
+
+GEMINI_API_KEY: "AQ.Ab8RN6L6AoZalnQ0nEl4Y_3c5LbkD7CG0mMHdUHqOBNFFht92w",  
+OPENAI_API_KEY: "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",  
+CEREBRAS_API_KEY: "csk-wppydn6nkr9w2p9699hvhrpc54nynkvyt8fn262rvmn2d9ky",  
+
+// Grok uses X.ai endpoints (if you have access)  
+GROK_API_KEY: "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"
+
+}
